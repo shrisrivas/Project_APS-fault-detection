e8bef7f3cedc4edfc74635548be7a2d9bbab93cd
SHRISTI PRIYA <95016932+shrisrivas@users.noreply.github.com>
2023-01-19 13:08:41 +0530

    Add files via upload

---
+++ data_dump.py
@@ -0,0 +1,26 @@
+import pymongo
+import pandas as pd
+import json
+
+# provide the mongodb localhost url to connect python to mongodb
+# client = pymongo.MongoClient("mongodb://localhost:27017")
+from sensor.config import mongo_client
+
+DATA_FILE_PATH=(r"C:\Users\Shristi\Desktop\Data Science\project\aps-fault-detection\data")
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    #insert converted json record to mongo db
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
+
